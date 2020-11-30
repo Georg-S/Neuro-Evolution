ece952dcded742a083279b3fc53ac61f4a33bb81
DESKTOP-3K85TKB\Georg <g.steinhauser1@web.de>
2020-11-30 21:14:00 +0100

    HNE specific test refactor

--- Tests/TEST_HNE.cpp
+++ Tests/TEST_HNE.cpp
@@ -2,169 +2,166 @@
 #include <HNE/HNE.h>
 #include <vector>
 
-using namespace std;
-using namespace nev;
 
-TEST(HNE, populationOf1CalculatesTheCorrectValue) {
+TEST(TEST_HNE, populationOf1CalculatesTheCorrectValue) {
 
-	vector<HistoricalGenotype> histGenotype;
-	vector<LinkGene> links;
-	vector<NeuronGene> neurons;
-	neurons.push_back(NeuronGene(NeuronType::bias, 0));
-	neurons.push_back(NeuronGene(NeuronType::input, 1));
-	neurons.push_back(NeuronGene(NeuronType::output, 2));
-	links.push_back(LinkGene(1, 2, 1, true, 0));
-	Genotype geno = Genotype(neurons, links,0);
+	std::vector<nev::HistoricalGenotype> histGenotype;
+	std::vector<nev::LinkGene> links;
+	std::vector<nev::NeuronGene> neurons;
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::bias, 0));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::input, 1));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::output, 2));
+	links.push_back(nev::LinkGene(1, 2, 1, true, 0));
+	nev::Genotype geno = nev::Genotype(neurons, links,0);
 	geno.createPhenotype();
 
-	histGenotype.push_back(HistoricalGenotype(geno, 1, 1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno, 1, 1));
 
-	HNE epoch = HNE(histGenotype, 1, 1);
+	nev::HNE hne = nev::HNE(histGenotype, 1, 1);
 
-	vector<vector<double>> outputs = epoch.getOutputsSnapshot(vector<double>{1.0});
+	std::vector<std::vector<double>> outputs = hne.getOutputsSnapshot(std::vector<double>{1.0});
 
 	EXPECT_DOUBLE_EQ(outputs[0][0], 0.99260845865571812);
 }
 
-TEST(HNE, populationOf2CalculatesTheCorrectValue) {
+TEST(TEST_HNE, populationOf2CalculatesTheCorrectValue) {
 
-	vector<HistoricalGenotype> histGenotype;
-	vector<LinkGene> links;
-	vector<NeuronGene> neurons;
-	neurons.push_back(NeuronGene(NeuronType::bias, 0));
-	neurons.push_back(NeuronGene(NeuronType::input, 1));
-	neurons.push_back(NeuronGene(NeuronType::output, 2));
-	links.push_back(LinkGene(1, 2, 1, true, 0));
-	Genotype geno = Genotype(neurons, links,0);
+	std::vector<nev::HistoricalGenotype> histGenotype;
+	std::vector<nev::LinkGene> links;
+	std::vector<nev::NeuronGene> neurons;
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::bias, 0));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::input, 1));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::output, 2));
+	links.push_back(nev::LinkGene(1, 2, 1, true, 0));
+	nev::Genotype geno = nev::Genotype(neurons, links,0);
 
-	histGenotype.push_back(HistoricalGenotype(geno,1,1));
-	histGenotype.push_back(HistoricalGenotype(geno,1,1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno,1,1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno,1,1));
 
-	HNE epoch = HNE(histGenotype,1,1);
+	nev::HNE hne = nev::HNE(histGenotype,1,1);
 
-	vector<vector<double>> outputs = epoch.getOutputsSnapshot(vector<double>{1.0});
+	std::vector<std::vector<double>> outputs = hne.getOutputsSnapshot(std::vector<double>{1.0});
 	
-	for (int i = 0; i < outputs.size(); i++) {
+	for (int i = 0; i < outputs.size(); i++)
 		EXPECT_DOUBLE_EQ(outputs[i][0], 0.99260845865571812);
-	}
 }
 
-TEST(HNE, populationOf3CalculatesTheCorrectValue) {
-	vector<HistoricalGenotype> histGenotype;
-	vector<LinkGene> links;
-	vector<NeuronGene> neurons;
-	neurons.push_back(NeuronGene(NeuronType::bias, 0));
-	neurons.push_back(NeuronGene(NeuronType::input, 1));
-	neurons.push_back(NeuronGene(NeuronType::output, 2));
-	links.push_back(LinkGene(1, 2, 1, true, 0));
-	Genotype geno = Genotype(neurons, links,0);
+TEST(TEST_HNE, populationOf3CalculatesTheCorrectValue) {
+	std::vector<nev::HistoricalGenotype> histGenotype;
+	std::vector<nev::LinkGene> links;
+	std::vector<nev::NeuronGene> neurons;
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::bias, 0));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::input, 1));
+	neurons.push_back(nev::NeuronGene(nev::NeuronType::output, 2));
+	links.push_back(nev::LinkGene(1, 2, 1, true, 0));
+	nev::Genotype geno = nev::Genotype(neurons, links,0);
 
-	histGenotype.push_back(HistoricalGenotype(geno,1,1));
-	histGenotype.push_back(HistoricalGenotype(geno,1,1));
-	histGenotype.push_back(HistoricalGenotype(geno,1,1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno,1,1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno,1,1));
+	histGenotype.push_back(nev::HistoricalGenotype(geno,1,1));
 
-	HNE epoch = HNE(histGenotype,1,1);
+	nev::HNE hne = nev::HNE(histGenotype,1,1);
 
-	vector<vector<double>> outputs = epoch.getOutputsSnapshot(vector<double>{1.0});
+	std::vector<std::vector<double>> outputs = hne.getOutputsSnapshot(std::vector<double>{1.0});
 	
-	for (int i = 0; i < outputs.size(); i++) {
+	for (int i = 0; i < outputs.size(); i++)
 		EXPECT_DOUBLE_EQ(outputs[i][0], 0.99260845865571812);
-	}
 }
 
 
-TEST(HNE, populationOf2GetsResetedAfterCenturyEnd) {
-	HNE epoch = HNE(2,2,1, 1);
-	epoch.setHighestFitnessAtStartOfCentury(1);
-	epoch.iterateOneGeneration(vector<double>{1, 1});
-	epoch.iterateOneGeneration(vector<double>{1, 1});
-	epoch.iterateOneGeneration(vector<double>{1, 1});
+TEST(TEST_HNE, populationOf2GetsResetedAfterCenturyEnd) {
+	nev::HNE hne = nev::HNE(2,2,1, 1);
+	hne.setHighestFitnessAtStartOfCentury(1);
+	hne.iterateOneGeneration(std::vector<double>{1, 1});
+	hne.iterateOneGeneration(std::vector<double>{1, 1});
+	hne.iterateOneGeneration(std::vector<double>{1, 1});
 }
 
-TEST(HNE, calculateElitismCount0PercentReturnsCorrectResult) {
-	HNE epoch = HNE(2, 10, 1, 1);
-	epoch.setElitismPercentage(0);
+TEST(TEST_HNE, calculateElitismCount0PercentReturnsCorrectResult) {
+	nev::HNE hne = nev::HNE(2, 10, 1, 1);
+	hne.setElitismPercentage(0);
 
-	EXPECT_EQ(0, epoch.calculateElitismCount());
+	EXPECT_EQ(0, hne.calculateElitismCount());
 }
-TEST(HNE, calculateElitismCount10PercentReturnsCorrectResult) {
-	HNE epoch = HNE(2, 10, 1, 1);
-	epoch.setElitismPercentage(0.1);
+TEST(TEST_HNE, calculateElitismCount10PercentReturnsCorrectResult) {
+	nev::HNE hne = nev::HNE(2, 10, 1, 1);
+	hne.setElitismPercentage(0.1);
 	
-	EXPECT_EQ(1, epoch.calculateElitismCount());
+	EXPECT_EQ(1, hne.calculateElitismCount());
 }
-TEST(HNE, calculateElitismCount33PercentReturnsCorrectResult) {
-	HNE epoch = HNE(2, 10, 1, 1);
-	epoch.setElitismPercentage(0.33);
+TEST(TEST_HNE, calculateElitismCount33PercentReturnsCorrectResult) {
+	nev::HNE hne = nev::HNE(2, 10, 1, 1);
+	hne.setElitismPercentage(0.33);
 
-	EXPECT_EQ(3, epoch.calculateElitismCount());
-}
-
-TEST(HNE, calculateElitismCount66PercentReturnsCorrectResult) {
-	HNE epoch = HNE(2, 10, 1, 1);
-	epoch.setElitismPercentage(0.66);
-
-	EXPECT_EQ(6, epoch.calculateElitismCount());
+	EXPECT_EQ(3, hne.calculateElitismCount());
 }
 
+TEST(TEST_HNE, calculateElitismCount66PercentReturnsCorrectResult) {
+	nev::HNE hne = nev::HNE(2, 10, 1, 1);
+	hne.setElitismPercentage(0.66);
 
-TEST(HNE, theGenotypesGetPurgedIfNotEnoughImprovementIsMade) {
-	HNE epoch = HNE(2, 2, 1, 1);
-	epoch.setElitismPercentage(0.0);
-	epoch.setHighestFitnessAtStartOfCentury(1);
-	epoch.iterateOneGeneration(vector<double> {1, 1});
-	epoch.iterateOneGeneration(vector<double> {1, 1});
-	epoch.iterateOneGeneration(vector<double> {1, 1});
-
-	EXPECT_EQ(0, epoch.getHighestFitness());
+	EXPECT_EQ(6, hne.calculateElitismCount());
 }
 
-TEST(HNE, theGenotypesGetsNotPurgedIfEnoughImprovementIsMade) {
-	HNE epoch = HNE(2, 2, 1, 1);;
-	epoch.setElitismPercentage(0.0);
-	epoch.setHighestFitnessAtStartOfCentury(1);
-	epoch.iterateOneGeneration(vector<double> {1.2, 1.2});
-	epoch.iterateOneGeneration(vector<double> {1.2, 1.2});
-	epoch.iterateOneGeneration(vector<double> {1.2, 1.2});
 
-	EXPECT_EQ(1.2, epoch.getHighestFitness());
+TEST(TEST_HNE, theGenotypesGetPurgedIfNotEnoughImprovementIsMade) {
+	nev::HNE hne = nev::HNE(2, 2, 1, 1);
+	hne.setElitismPercentage(0.0);
+	hne.setHighestFitnessAtStartOfCentury(1);
+	hne.iterateOneGeneration(std::vector<double> {1, 1});
+	hne.iterateOneGeneration(std::vector<double> {1, 1});
+	hne.iterateOneGeneration(std::vector<double> {1, 1});
+
+	EXPECT_EQ(0, hne.getHighestFitness());
 }
 
-TEST(HNE, theEliteDoesntGetPurgedAfterCenturyEnd) {
-	HNE epoch = HNE(2, 2, 1, 1);;
-	epoch.setElitismPercentage(0.5);
-	epoch.setHighestFitnessAtStartOfCentury(1);
-	epoch.iterateOneGeneration(vector<double> {1.05, 1.0});
-	epoch.iterateOneGeneration(vector<double> {1.05, 1.0});
-	epoch.iterateOneGeneration(vector<double> {1.05, 1.0});
+TEST(TEST_HNE, theGenotypesGetsNotPurgedIfEnoughImprovementIsMade) {
+	nev::HNE hne = nev::HNE(2, 2, 1, 1);;
+	hne.setElitismPercentage(0.0);
+	hne.setHighestFitnessAtStartOfCentury(1);
+	hne.iterateOneGeneration(std::vector<double> {1.2, 1.2});
+	hne.iterateOneGeneration(std::vector<double> {1.2, 1.2});
+	hne.iterateOneGeneration(std::vector<double> {1.2, 1.2});
 
-	EXPECT_EQ(1.05, epoch.getHighestFitness());
+	EXPECT_EQ(1.2, hne.getHighestFitness());
 }
 
-TEST(HNE, theEliteDoesntGetPurgedAndReturnsTheSameOutput) {
-	HNE epoch = HNE(3, 2, 1, 1);;
-	epoch.setElitismPercentage(0.5);
-	epoch.setHighestFitnessAtStartOfCentury(1);
+TEST(TEST_HNE, theEliteDoesntGetPurgedAfterCenturyEnd) {
+	nev::HNE hne = nev::HNE(2, 2, 1, 1);;
+	hne.setElitismPercentage(0.5);
+	hne.setHighestFitnessAtStartOfCentury(1);
+	hne.iterateOneGeneration(std::vector<double> {1.05, 1.0});
+	hne.iterateOneGeneration(std::vector<double> {1.05, 1.0});
+	hne.iterateOneGeneration(std::vector<double> {1.05, 1.0});
+
+	EXPECT_EQ(1.05, hne.getHighestFitness());
+}
+
+TEST(TEST_HNE, theEliteDoesntGetPurgedAndReturnsTheSameOutput) {
+	nev::HNE hne = nev::HNE(3, 2, 1, 1);;
+	hne.setElitismPercentage(0.5);
+	hne.setHighestFitnessAtStartOfCentury(1);
 	for (int i = 0; i < 100; i++) {
-		vector <vector<double>> outputs = epoch.getOutputsSnapshot(vector<double> {1.0});
+		std::vector <std::vector<double>> outputs = hne.getOutputsSnapshot(std::vector<double> {1.0});
 		double previousOutput = outputs[0][0];
-		epoch.iterateOneGeneration(vector<double> {1.05, 1.0,1.0});
-		epoch.iterateOneGeneration(vector<double> {1.00, 1.0,1.0});
-		epoch.iterateOneGeneration(vector<double> {1.00, 1.0 , 1.0});
-		outputs = epoch.getOutputsSnapshot(vector<double> {1.0});
+		hne.iterateOneGeneration(std::vector<double> {1.05, 1.0,1.0});
+		hne.iterateOneGeneration(std::vector<double> {1.00, 1.0,1.0});
+		hne.iterateOneGeneration(std::vector<double> {1.00, 1.0 , 1.0});
+		outputs = hne.getOutputsSnapshot(std::vector<double> {1.0});
 		double currentOutput = outputs[0][0];
 
 		EXPECT_EQ(previousOutput, currentOutput);
 	}
 }
 
-TEST(Epoch, theOutputChangesAfterIteration) {
-	HNE epoch = HNE(10, 1, 1, 1);;
-	epoch.setMutateWeightProbability(1.0);
-	vector<vector<double>> results = epoch.getOutputsSnapshot(vector<double>{1.0});
+TEST(TEST_HNE, theOutputChangesAfterIteration) {
+	nev::HNE hne = nev::HNE(10, 1, 1, 1);;
+	hne.setMutateWeightProbability(1.0);
+	hne.setNewWeightProbability(1.0);
+	std::vector<std::vector<double>> results = hne.getOutputsSnapshot(std::vector<double>{1.0});
 	double previousResult = results[0][0];
-	epoch.iterateOneGeneration(vector<double> {1.0});
-	results = epoch.getOutputsSnapshot(vector<double>{1.0});
+	hne.iterateOneGeneration(std::vector<double> {1.0});
+	results = hne.getOutputsSnapshot(std::vector<double>{1.0});
 	double currenResult = results[0][0];
 
 	EXPECT_NE(previousResult, currenResult);
--- Tests/TEST_HistoricalGenotype.cpp
+++ Tests/TEST_HistoricalGenotype.cpp
@@ -1,19 +1,17 @@
 #include <gtest/gtest.h>
 #include <HNE/HNE.h>
 
-using namespace std;
-using namespace nev;
 
-TEST(HistoricalGenotype, HistorySizeIs1AfterCreation) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno, 1, 1);
+TEST(TEST_HistoricalGenotype, HistorySizeIs1AfterCreation) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno, 1, 1);
 	genotype.setFitness(1.0);
 	EXPECT_EQ(genotype.getHistorySize(), 1);
 }
 
-TEST(HistoricalGenotype, HistorySizeIncreasesAfterIteration) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno,1, 1);
+TEST(TEST_HistoricalGenotype, HistorySizeIncreasesAfterIteration) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno,1, 1);
 	genotype.setFitness(1.0);
 	EXPECT_EQ(genotype.getHistorySize(), 1);
 	genotype.iterate(inno);
@@ -21,9 +19,9 @@ TEST(HistoricalGenotype, HistorySizeIncreasesAfterIteration) {
 	EXPECT_EQ(genotype.getHistorySize(), 2);
 }
 
-TEST(HistoricalGenotype, HistoricalGenotypeResetsIfNotEnoughImprovementIsMade) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno, 1, 1);
+TEST(TEST_HistoricalGenotype, HistoricalGenotypeResetsIfNotEnoughImprovementIsMade) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno, 1, 1);
 	genotype.setHighestFitnessAtStartOfCentury(1.0);
 	genotype.iterate(inno);
 	genotype.setFitness(1.05);
@@ -32,9 +30,9 @@ TEST(HistoricalGenotype, HistoricalGenotypeResetsIfNotEnoughImprovementIsMade) {
 	EXPECT_EQ(genotype.getHighestFitness(), 0);
 }
 
-TEST(HistoricalGenotype, HistoricalGenotypeSurvivesIfEnoughImprovementIsMade) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno,1, 1);
+TEST(TEST_HistoricalGenotype, HistoricalGenotypeSurvivesIfEnoughImprovementIsMade) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno,1, 1);
 	genotype.setHighestFitnessAtStartOfCentury(1.0);
 	genotype.iterate(inno);
 	genotype.setFitness(1.1);
@@ -43,9 +41,9 @@ TEST(HistoricalGenotype, HistoricalGenotypeSurvivesIfEnoughImprovementIsMade) {
 	EXPECT_EQ(genotype.getHighestFitness(), 1.1);
 }
 
-TEST(HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvolutionaryPurge) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno, 1, 1);
+TEST(TEST_HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvolutionaryPurge) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno, 1, 1);
 	genotype.setHighestFitnessAtStartOfCentury(1.0);
 	genotype.iterate(inno);
 	genotype.setFitness(1.1);
@@ -54,9 +52,9 @@ TEST(HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvol
 	EXPECT_EQ(genotype.getHighestFitnessAtStartOfCentury(), 1.1);
 }
 
-TEST(HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvolutionReset) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno, 1, 1);
+TEST(TEST_HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvolutionReset) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno, 1, 1);
 	genotype.setHighestFitnessAtStartOfCentury(1.0);
 	genotype.iterate(inno);
 	genotype.setFitness(1.05);
@@ -65,15 +63,16 @@ TEST(HistoricalGenotype, highestFitnessAtStartOfCenturyGetsSetCorrectlyAfterEvol
 	EXPECT_EQ(genotype.getHighestFitnessAtStartOfCentury(), 0);
 }
 
-TEST(HistoricalGenotype, outputChangesAfterMutation) {
-	Innovation inno;
-	HistoricalGenotype genotype = HistoricalGenotype(inno, 1, 1);
+TEST(TEST_HistoricalGenotype, outputChangesAfterMutation) {
+	nev::Innovation inno;
+	nev::HistoricalGenotype genotype = nev::HistoricalGenotype(inno, 1, 1);
 	genotype.setMutateWeightProbability(1);
-	vector<double> outputs = genotype.calculateOutputSnapshotFromLastGenotype(vector<double> {1.0});
+	genotype.setNewWeightProbability(1);
+	std::vector<double> outputs = genotype.calculateOutputSnapshotFromLastGenotype(std::vector<double> {1.0});
 	double previousValue = outputs[0];
 	genotype.iterate(inno);
 	outputs.clear();
-	outputs = genotype.calculateOutputSnapshotFromLastGenotype(vector<double> {1.0});
+	outputs = genotype.calculateOutputSnapshotFromLastGenotype(std::vector<double> {1.0});
 
 	EXPECT_NE(previousValue, outputs[0]);
 }
