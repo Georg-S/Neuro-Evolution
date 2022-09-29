178c0af33b05034dcf7fd7b9b542780e053958d6
Gork <g.steinhauser1@web.de>
2022-09-29 22:21:21 +0200

    Refactor unit tests for NEAT

--- Tests/TEST_NEAT.cpp
+++ Tests/TEST_NEAT.cpp
@@ -5,62 +5,74 @@
 
 #if RUN_NORMAL_TESTS
 
-TEST(TEST_NEAT, constructorCreatesRightAmountOfGenotypes) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, constructorCreatesRightAmountOfGenotypes)
+{
+	auto neat = nev::NEAT(50, 1, 1);
+
 	EXPECT_EQ(neat.getPopulationSize(), 50);
 }
 
-TEST(TEST_NEAT, afterConstructionReturnsTheRightHighestGenotypeID) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, afterConstructionReturnsTheRightHighestGenotypeID)
+{
+	auto neat = nev::NEAT(50, 1, 1);
+
 	EXPECT_EQ(neat.getHighestGenotypeId(), 49);
 }
 
-TEST(TEST_NEAT, afterConstructionA1By1Net2InnovationsAreCreated) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, afterConstructionA1By1Net2InnovationsAreCreated)
+{
+	auto neat = nev::NEAT(50, 1, 1);
+
 	EXPECT_EQ(neat.getTotalCountOfInnovations(), 2);
 }
 
-TEST(TEST_NEAT, calculateOutputSnapshotReturnsRightAmountOfOutputs) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
-	std::vector<std::vector<double>> outputs = neat.calculateOutputSnapshot(std::vector<double>{1.0});
+TEST(TEST_NEAT, calculateOutputSnapshotReturnsRightAmountOfOutputs)
+{
+	auto neat = nev::NEAT(50, 1, 1);
+	auto outputs = neat.calculateOutputSnapshot(std::vector<double>{1.0});
+
 	EXPECT_EQ(outputs.size(), 50);
 }
 
-TEST(TEST_NEAT, afterOneIterationNoSpeciesIsCreatedIfNotEnoughFitnessValuesArePassed) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, afterOneIterationNoSpeciesIsCreatedIfNotEnoughFitnessValuesArePassed)
+{
+	auto neat = nev::NEAT(50, 1, 1);
 	std::vector<double> fitness;
 	for (int i = 0; i < 10; i++)
-		fitness.push_back(1.0);
+		fitness.emplace_back(1.0);
 	neat.iterateOneGeneration(fitness);
 
 	EXPECT_EQ(neat.getSpeciesCount(), 0);
 }
 
-TEST(TEST_NEAT, afterOneIterationASpeciesIsCreated) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, afterOneIterationASpeciesIsCreated)
+{
+	auto neat = nev::NEAT(50, 1, 1);
 	std::vector<double> fitness;
 	for (int i = 0; i < 50; i++)
-		fitness.push_back(1.0);
+		fitness.emplace_back(1.0);
 	neat.iterateOneGeneration(fitness);
 
 	EXPECT_EQ(neat.getSpeciesCount(), 1);
 }
 
-TEST(TEST_NEAT, afterOneIterationPopulationSizeIsTheSame) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, afterOneIterationPopulationSizeIsTheSame)
+{
+	auto neat = nev::NEAT(50, 1, 1);
 	std::vector<double> fitness;
 	for (int i = 0; i < 50; i++)
-		fitness.push_back(1.0);
+		fitness.emplace_back(1.0);
 	neat.iterateOneGeneration(fitness);
 
 	EXPECT_EQ(neat.getPopulationSize(), 50);
 }
 
-TEST(TEST_NEAT, after10IterationPopulationSizeIsTheSame) {
-	nev::NEAT neat = nev::NEAT(50, 1, 1);
+TEST(TEST_NEAT, after10IterationPopulationSizeIsTheSame)
+{
+	auto neat = nev::NEAT(50, 1, 1);
 	std::vector<double> fitness;
 	for (int i = 0; i < 50; i++)
-		fitness.push_back(1.0);
+		fitness.emplace_back(1.0);
 
 	for (int i = 0; i < 10; i++)
 		neat.iterateOneGeneration(fitness);
